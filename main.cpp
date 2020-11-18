58e9a656e368c758401ece43750c97b3ec6e1ef3
NaviHX <932942142@qq.com>
2020-11-18 22:02:25 +0800

    添加注释

--- main.cpp
+++ main.cpp
@@ -13,11 +13,11 @@ typedef struct BinaryTreeNode //二叉树节点
     char val;                     //权值
 } node;
 
-node *nodeInit(node *left, node *right, char val);
-node *binaryTreeBuild(string &s, int *idx);
-void binaryTreeFree(node *root);
-void binaryTreePrint(node *root);
-int getBinaryTreeDepth(node *root);
+node *nodeInit(node *left, node *right, char val); //得到并初始化一个节点
+node *binaryTreeBuild(string &s, int *idx);        //递归建立二叉树
+void binaryTreeFree(node *root);                   //释放二叉树
+void binaryTreePrint(node *root);                  //打印二叉树
+int getBinaryTreeDepth(node *root);                //获得二叉树深度
 void test();
 void testPrint(node *root);
 
@@ -112,19 +112,21 @@ void binaryTreePrint(node *root)
 {
     int level = getBinaryTreeDepth(root), count = 0, curLevel = 1, levelSize = 1, sumSize = 1;
     int loc = 0;
-    queue<node *> q;
-    queue<int> qloc;
+    queue<node *> q; //层序遍历队列
+    queue<int> qloc; //计算每个节点应该打印的位置
     q.push(root);
     while (curLevel <= level)
     {
-        int dis = (1 << (level - curLevel));
+        int dis = (1 << (level - curLevel)); //计算间隔
         node *temp = q.front();
         q.pop();
         count++;
         cout << (temp == NULL ? ' ' : temp->val);
-        if (!temp)
+        if (!temp) //空节点
         {
-            q.push((node *)NULL);
+            //添加两个空子节点
+            //相应位置输出空格
+            q.push((node *)NULL); 
             q.push((node *)NULL);
             for (int i = 0; i < dis; i++)
                 cout << " ";
@@ -134,6 +136,7 @@ void binaryTreePrint(node *root)
         }
         else
         {
+            //压入左右子节点
             q.push(temp->left);
             q.push(temp->right);
             if (temp->left)
@@ -164,6 +167,7 @@ void binaryTreePrint(node *root)
         }
         if (count == sumSize)
         {
+            //处理向下的连线
             levelSize *= 2;
             sumSize += levelSize;
             curLevel++;
